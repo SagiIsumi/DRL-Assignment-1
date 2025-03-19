9f3b0bfbad25d5f27139f8b0e8ff0c2df7e6ef33
SagiIsumi <saginomiyaisumi0924@gmail.com>
2025-03-19 20:59:19 +0800

    second upload for new sampling strategy

--- student_agent.py
+++ student_agent.py
@@ -13,18 +13,18 @@ def get_key_state(state):
     key_state=f"({state[0]},{state[1]})_({state[2]},{state[3]})_({state[4]},{state[5]})_({state[6]},{state[7]})_({state[8]},{state[9]})_({state[10]},{state[11]},{state[12]},{state[13]})_{state[14]}_{state[15]}"
     return key_state
 def get_action(obs):
-    while True:
+    for i in range(50):
         # TODO: Train your own agent
         key_state=get_key_state(obs)
         if key_state not in q.keys():
             taxi_row, taxi_col, _,_,_,_,_,_,_,_,obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look = obs
             obs=[taxi_row, taxi_col,random.randint(5,10),random.randint(5,10),random.randint(5,10),random.randint(5,10),\
                 random.randint(5,10),random.randint(5,10),random.randint(5,10),random.randint(5,10),obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look]
-            continue
         else:
             action_probs = softmax(q[key_state])
             action = np.random.choice(6, p=action_probs)
             return action # Choose a random action
+        return random.choice([0,1,2,3,4,5])
     # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
