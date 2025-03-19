78287728a8cb50e7181f157fc63834496f46f0f0
SagiIsumi <saginomiyaisumi0924@gmail.com>
2025-03-19 19:43:46 +0800

    new action sampling strategy

--- student_agent.py
+++ student_agent.py
@@ -13,21 +13,25 @@ def get_key_state(state):
     key_state=f"({state[0]},{state[1]})_({state[2]},{state[3]})_({state[4]},{state[5]})_({state[6]},{state[7]})_({state[8]},{state[9]})_({state[10]},{state[11]},{state[12]},{state[13]})_{state[14]}_{state[15]}"
     return key_state
 def get_action(obs):
-    
-    # TODO: Train your own agent
-    key_state=get_key_state(obs)
-    if key_state not in q.keys():
-        action=random.choice([0, 1, 2, 3, 4, 5])
-    else:
-        action_probs = softmax(q[key_state])
-        action = np.random.choice(6, p=action_probs)
+    while True:
+        # TODO: Train your own agent
+        key_state=get_key_state(obs)
+        if key_state not in q.keys():
+            taxi_row, taxi_col, _,_,_,_,_,_,_,_,obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look = obs
+            obs=[taxi_row, taxi_col,random.randint(5,10),random.randint(5,10),random.randint(5,10),random.randint(5,10),\
+                random.randint(5,10),random.randint(5,10),random.randint(5,10),random.randint(5,10),obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look]
+            continue
+        else:
+            action_probs = softmax(q[key_state])
+            action = np.random.choice(6, p=action_probs)
+            return action # Choose a random action
     # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
     
 
-    return action # Choose a random action
+    
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 if __name__=="__main__":
     print(q)
